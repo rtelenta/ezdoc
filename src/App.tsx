1c32baf3836cb1c98796dc6954726115c52fb9fc
Renzo Telenta <renzotelenta@gmail.com>
2025-11-22 17:03:08 -0500

    feat: integrate Tailwind CSS and Ant Design into the project
    - Added Tailwind CSS and Ant Design as dependencies in package.json
    - Updated App component to use Ant Design's Button and Tailwind CSS for styling
    - Removed old CSS styles and imported Tailwind CSS in index.css
    - Configured Vite to use Tailwind CSS plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
-import { useState } from "react";
+import { Button } from "antd";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <h1>Vite + React + TypeScript</h1>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is: {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>Click on the Vite and React logos to learn more.</p>
-      </div>
-    </>
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button type="primary">Button</Button>
+    </div>
   );
 }
 
